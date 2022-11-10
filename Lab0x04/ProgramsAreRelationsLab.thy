89d0c8e495559b9c703e81c144baed2bc5da47fd
Carevic <filip.carevic@epfl.ch>
2022-11-10 19:30:26 +0100

    finished, needs refactoring

--- Lab0x04/ProgramsAreRelationsLab.thy
+++ Lab0x04/ProgramsAreRelationsLab.thy
@@ -6,8 +6,21 @@ section \<open> Start by writing your solution to problem 2.10 of
 http://concrete-semantics.org/concrete-semantics.pdf\<close>
 
 
+datatype tree0 =  Leaf| Node "tree0"  "tree0"
+
+fun nodes ::"tree0 \<Rightarrow> nat" where
+"nodes (Node l r) = (nodes l) + (nodes r) + 1"
+|
+"nodes Leaf = 1"
 
 
+fun explode:: "nat \<Rightarrow> tree0 \<Rightarrow> tree0" where
+"explode 0 t=t" |
+"explode (Suc n) t=explode n (Node t t)"
+
+lemma aft_tree: "nodes(explode n t) = (nodes t + 1)*(2^n) - 1"
+apply(induction n arbitrary: t)
+by auto(simp add: algebra_simps)
 
 section \<open> See e.g. src/HOL/Relations.thy for properties of relations
 
