49a1aac402913363bce4b5e53d1fed48ba261d18
Natalija Mitic <naca@ntec.ch>
2022-12-08 17:51:52 +0100

    remove height attribute

--- Project/AVL.scala
+++ Project/AVL.scala
@@ -13,17 +13,17 @@ object AVL {
         
         def getKeySet: Set[BigInt] = this match {
             case Empty() => Set.empty
-            case Node(k, l, r, _) => l.getKeySet ++ Set(k) ++ r.getKeySet
+            case Node(k, l, r) => l.getKeySet ++ Set(k) ++ r.getKeySet
         }
 
         def getKeyList: List[BigInt] = (this match {
             case Empty() => List.empty[BigInt]
-            case Node(k, l, r, _) => l.getKeyList ++ (k :: r.getKeyList)
+            case Node(k, l, r) => l.getKeyList ++ (k :: r.getKeyList)
         }).ensuring(_.content == this.getKeySet)
         
         lazy val size: BigInt = (this match {
             case Empty() => BigInt(0)
-            case Node(_, l, r, _) => l.size + 1 + r.size
+            case Node(_, l, r) => l.size + 1 + r.size
         }) ensuring(_ == getKeyList.length)
 
 
@@ -38,7 +38,7 @@ object AVL {
         def isBST: Boolean = {
             this match {
                 case Empty() => true
-                case Node(k, l, r, _) => l.checkGreatest(k) && r.checkSmallest(k) && l.isBST && r.isBST 
+                case Node(k, l, r) => l.checkGreatest(k) && r.checkSmallest(k) && l.isBST && r.isBST 
             }
         }
 
@@ -46,7 +46,7 @@ object AVL {
             require(isBST)
             this match {
                 case Empty() => false
-                case Node(value, left, right, _) =>
+                case Node(value, left, right) =>
                 if (searched == value) {
                     true
                 } else if(searched < value) {
@@ -61,7 +61,7 @@ object AVL {
             require(isAVL)
             this match {
                 case Empty() => false
-                case Node(value, left, right, _) =>
+                case Node(value, left, right) =>
                 if (searched == value) {
                     true
                 } else if(searched < value) {
@@ -75,27 +75,23 @@ object AVL {
         def insertBST(value: BigInt): Tree = {
             require(isBST)
             this match {
-                case Empty() => Node(value, Empty(), Empty(), 0)
-                case Node(v, l, r, h) => (if (v < value) {
+                case Empty() => Node(value, Empty(), Empty())
+                case Node(v, l, r) => (if (v < value) {
                     val nr = r.insertBST(value)
-                    Node(v, l, nr, getNewHeight(l,r) )
+                    Node(v, l, nr)
                 } else if (v > value) {
                     val nl = l.insertBST(value)
-                    Node(v, nl, r, getNewHeight(nl, r))
+                    Node(v, nl, r)
                 } else {
                     this
                 })
             }
         } ensuring(res => res.isBST && res.getKeySet == getKeySet ++ Set(value))
 
-        def getNewHeight(l:Tree, r:Tree):BigInt = {
-            stainless.math.max(l.height, r.height) + 1
-        }.ensuring(res => res > -1)
-
         def height: BigInt = (
             this match {
                 case Empty() => BigInt(-1)
-                case Node(k, l, r, h) => {
+                case Node(k, l, r) => {
                    stainless.math.max(l.height , r.height) + 1
                 }
             }
@@ -106,14 +102,14 @@ object AVL {
             decreases(size)
             this match {
                 case Empty() => true
-                case Node(_, l, r, h) => (l.height - r.height == 1 || r.height - l.height == 1 || r.height == l.height) && l.isBalanced && r.isBalanced // stainless.math.abs mozda nece da radi
+                case Node(_, l, r) => (l.height - r.height == 1 || r.height - l.height == 1 || r.height == l.height) && l.isBalanced && r.isBalanced // stainless.math.abs mozda nece da radi
             }
         }
 
         def isAVL: Boolean = {
             this match {
                 case Empty() => true
-                case Node(k, l, r, h) => isBalanced && isBST && r.isAVL && l.isAVL
+                case Node(k, l, r) => isBalanced && isBST && r.isAVL && l.isAVL
             }
         }
 
@@ -124,13 +120,13 @@ object AVL {
 
             if (l.height == r.height + 2) {
                 l match{
-                    case Node(ln, ll, lr, _) =>  {
+                    case Node(ln, ll, lr) =>  {
                         if (ll.height < lr.height) {
                             lr match {
-                                case Node(lrn, lrl, lrr, _) => {
+                                case Node(lrn, lrl, lrr) => {
 
-                                    val n1 = Node(ln,ll,lrl, getNewHeight(ll,lrl))
-                                    val n2 = Node(n,lrr,r, getNewHeight(lrr,r))
+                                    val n1 = Node(ln,ll,lrl)
+                                    val n2 = Node(n,lrr,r)
 
                                     // PROVE IS BST
 
@@ -153,13 +149,13 @@ object AVL {
                                     check(stainless.math.abs(n1.height - n2.height)<=1)
                                     check(n1.isBalanced)
                                     check(n2.isBalanced)
-                                    Node( lrn, n1 , n2, getNewHeight(n1,n2)) 
+                                    Node( lrn, n1 , n2) 
                                     }
                                 case Empty() => Empty() // Should never happen
                             }
                         } else {
-                            val n1 =  Node(n,lr,r, getNewHeight(lr,r))
-                            Node(ln, ll,n1 ,getNewHeight(ll,n1))
+                            val n1 =  Node(n,lr,r)
+                            Node(ln, ll,n1)
                         }
                     }
                     case Empty() => Empty() // Should never happen
@@ -167,7 +163,7 @@ object AVL {
             }else {
                 check(l.isAVL && r.isAVL)
                 check(stainless.math.abs(l.height - r.height) <=1)
-                Node(n, l, r, getNewHeight(l,r))
+                Node(n, l, r)
             }
         } 
         . ensuring (res => res.isAVL  && (res.size == l.size + r.size + 1)  && (res.height == stainless.math.max(l.height, r.height) +1 || res.height == stainless.math.max(l.height, r.height)) && l.getKeySet.subsetOf(res.getKeySet) && res.getKeySet.contains(n) && r.getKeySet.subsetOf(res.getKeySet))
@@ -179,12 +175,12 @@ object AVL {
 
             if (r.height == l.height + 2) {
                 r match{
-                    case Node(rn, rl, rr, _) =>  {
+                    case Node(rn, rl, rr) =>  {
                         if (rl.height > rr.height) {
                             rl match {
-                                case Node(rln, rll, rlr, _) => {
-                                    val n1 = Node(n,l,rll, getNewHeight(l,rll))
-                                    val n2 = Node(rn,rlr,rr, getNewHeight(rlr, rr))
+                                case Node(rln, rll, rlr) => {
+                                    val n1 = Node(n,l,rll)
+                                    val n2 = Node(rn,rlr,rr)
 
                                     // PROVE IS BST
                                     check(r.checkSmallest(n) ==> (n<rn && rll.checkSmallest(n)))
@@ -205,13 +201,13 @@ object AVL {
                                     check(stainless.math.abs(n1.height - n2.height)<=1)
                                     check(n1.isBalanced)
                                     check(n2.isBalanced)
-                                    Node( rln, n1,n2, getNewHeight(n1,n2))
+                                    Node( rln, n1,n2)
                                 }
                                 case Empty() => Empty() // Should never happen
                             }
                         } else {
-                            val n1 = Node(n,l,rl, getNewHeight(l,rl))
-                            Node(rn, n1, rr, getNewHeight(n1,rr))
+                            val n1 = Node(n,l,rl)
+                            Node(rn, n1, rr)
                         }
                     }
                     case Empty() => Empty() // Should never happen
@@ -219,7 +215,7 @@ object AVL {
             }else {
                 check(l.isAVL && r.isAVL)
                 check(stainless.math.abs(l.height - r.height) <=1)
-                Node(n, l, r, getNewHeight(l,r))
+                Node(n, l, r)
             }
         }
         .ensuring(res=> res.isAVL && (res.size == l.size + r.size + 1)  && (res.height == stainless.math.max(l.height, r.height) +1 || res.height == stainless.math.max(l.height, r.height)) && l.getKeySet.subsetOf(res.getKeySet) && res.getKeySet.contains(n) && r.getKeySet.subsetOf(res.getKeySet))
@@ -228,8 +224,8 @@ object AVL {
         def insertAVL(new_key: BigInt):Tree = {
             require(isAVL)
             this match {
-                case Empty() => Node(new_key, Empty(), Empty(), 0)
-                case Node(k, l, r, _) => {
+                case Empty() => Node(new_key, Empty(), Empty())
+                case Node(k, l, r) => {
                     if (new_key == k ){
                         this
                     } else if (new_key < k) {
@@ -237,14 +233,13 @@ object AVL {
                         val ll = l.insertAVL(new_key)
                         if(ll.height <= r.height +2) {
                             this // should never happen
-                        }
-                        else {
-                        check(stainless.math.abs(ll.height - l.height) <=1)
-                        check(ll.isAVL && r.isAVL)
-                        check(stainless.math.abs(l.height - r.height) <=1)
-                        check(ll.height <= r.height +2)
-                        check(((stainless.math.abs(ll.height - r.height) <=1) || ( ll.height == r.height+2)))
-                        balanceLeft(k, ll , r)
+                        } else {
+                            check(stainless.math.abs(ll.height - l.height) <=1)
+                            check(ll.isAVL && r.isAVL)
+                            check(stainless.math.abs(l.height - r.height) <=1)
+                            check(ll.height <= r.height +2)
+                            check(((stainless.math.abs(ll.height - r.height) <=1) || ( ll.height == r.height+2)))
+                            balanceLeft(k, ll , r)
                         }
                  
                     } else {
@@ -252,17 +247,14 @@ object AVL {
                         val rr = r.insertAVL(new_key)
                         if (rr.height <= l.height +2) {
                             this // should never happen
+                        } else {
+                            check(stainless.math.abs(rr.height - r.height) <=1)
+                            check(rr.isAVL && l.isAVL)
+                            check(stainless.math.abs(r.height - l.height) <=1)
+                            check(rr.height <= l.height +2)
+                            check(((stainless.math.abs(rr.height - l.height) <=1) || ( rr.height == l.height+2)))
+                            balanceRight(k, l , rr)
                         }
-                        else {
-                        check(stainless.math.abs(rr.height - r.height) <=1)
-                        check(rr.isAVL && l.isAVL)
-                        check(stainless.math.abs(r.height - l.height) <=1)
-                        check(rr.height <= l.height +2)
-                        check(((stainless.math.abs(rr.height - l.height) <=1) || ( rr.height == l.height+2)))
-                        balanceRight(k, l , rr)
-
-                        }
-                        
                     }
                 }
             }
@@ -272,7 +264,7 @@ object AVL {
         def delete_max(): (BigInt, Tree) = {
             require(this.size > 0 && isAVL)
             this match {
-                case Node(n, l, r, h) if r.size == 0 => {
+                case Node(n, l, r) if r.size == 0 => {
                     check(r.height == -1)
                     check(stainless.math.abs(r.height - l.height)<=1)
                     check(l.height == -1 || l.height == 0)
@@ -280,7 +272,7 @@ object AVL {
                     check(r.size == 0 && (l.size + 1) == this.size)
                     (n,l)
                 }
-                case Node(n, l, r ,h) => {
+                case Node(n, l, r) => {
                     val (n_prim, r_prim) = r.delete_max()
                     check(r.getKeySet.contains(n_prim))
                     check(l.checkGreatest(n) && r_prim.checkSmallest(n))
@@ -296,9 +288,9 @@ object AVL {
         def delete_root(): Tree = { 
             require(size > 0 && isAVL)
             this match {
-                case Node(n, l, r ,h ) if r.size == 0 => l
-                case Node(n, l, r ,h ) if l.size == 0 => r
-                case Node(n, l, r ,h ) => {
+                case Node(n, l, r) if r.size == 0 => l
+                case Node(n, l, r) if l.size == 0 => r
+                case Node(n, l, r) => {
                     val (n_prim, l_prim) = l.delete_max()
                     check(l_prim.size + 1 == l.size)
 
@@ -316,7 +308,7 @@ object AVL {
             require(isAVL)
             this match {
                 case Empty() => Empty()
-                case Node(n, l, r, h) => {
+                case Node(n, l, r) => {
                     if (key == n) {
                         delete_root()
                     } else if (key < n) {
@@ -344,9 +336,9 @@ object AVL {
         def joinRightAVL(tl: Tree, k:BigInt, tr:Tree):Tree = {
             require(tl.size > 0 && tr.size > 0 && tl.isAVL && tr.isAVL && tl.checkGreatest(k) && tr.checkSmallest(k) && tl.height > tr.height + 1)
             tl match {
-                case Node(kprim, l, r, _) => {
+                case Node(kprim, l, r) => {
                     if (r.height <= tr.height + 1) {
-                        val tprim = Node(k,r,tr,getNewHeight(r,tr))
+                        val tprim = Node(k,r,tr)
                         
                         check(l.checkGreatest(kprim))
                         check(r.checkSmallest(kprim) ==>  tprim.checkSmallest(kprim))
@@ -389,9 +381,9 @@ object AVL {
         def joinLeftAVL(tl: Tree, k:BigInt, tr:Tree):Tree = {
             require(tl.size > 0 && tr.size > 0 && tl.isAVL && tr.isAVL && tl.checkGreatest(k) && tr.checkSmallest(k) && tl.height + 1 < tr.height)
             tr match {
-                case Node(kprim, l, r, _) => {
+                case Node(kprim, l, r) => {
                     if (l.height <= tl.height + 1) {
-                        val tprim = Node(k,tl, l,getNewHeight(tl,l))
+                        val tprim = Node(k,tl, l)
                      
                         check(tl.checkGreatest(k))
                         check(l.checkSmallest(k))
@@ -434,12 +426,12 @@ object AVL {
             } else if (tr.height > tl.height + 1) {
                 joinLeftAVL(tl, k, tr)
             } else {
-                Node(k, tl, tr, getNewHeight(tl, tr))
+                Node(k, tl, tr)
             }
         }.ensuring(res=> res.isAVL && res.size == tl.size + tr.size + 1 && tl.getKeySet.subsetOf(res.getKeySet) && res.getKeySet.contains(k) && tr.getKeySet.subsetOf(res.getKeySet))
     
     }
  
-    case class Node(key: BigInt, left: Tree, right: Tree, hgt: BigInt) extends Tree
+    case class Node(key: BigInt, left: Tree, right: Tree) extends Tree
     case class Empty() extends Tree
 }
