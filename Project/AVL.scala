62520a4536dce8b5cb777469752030ebc5ebf701
Natalija Mitic <naca@ntec.ch>
2023-01-07 09:15:30 +0100

    format

--- Project/AVL.scala
+++ Project/AVL.scala
@@ -6,13 +6,13 @@ import stainless.annotation._
 import project.AVLOperations
 import project.AVL._
 
-case class AVLTree private(val tree: Tree) extends AVLOperations {
-    require(tree.isAVL) 
-    
+case class AVLTree private (val tree: Tree) extends AVLOperations {
+    require(tree.isAVL)
+
     override def lookup(element: BigInt): Boolean = lookupAVL(tree, element)
     override def insert(element: BigInt): Tree = insertAVL(tree, element)
     override def delete(element: BigInt): Tree = deleteAVL(tree, element)
-    
+
     override def split(element: BigInt): (Tree, Boolean, Tree) = splitAVL(tree, element)
     override def join(tl: Tree, k: BigInt, tr: Tree): Tree = {
         if (tl.isAVL && tr.isAVL && tl.checkGreatest(k) && tr.checkSmallest(k)) then {
